85dc2c77635856a6c3cee49d0c511e91cc5eb635
GitVHV <datnt@vhv.vn>
2022-05-20 11:14:59 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import time
+import streamlit as st
+import SessionState
+import numpy as np
+from predictor import Predictor
+from dataset.add_noise import SynthesizeData
+
+state = SessionState.get(text_correct="", input="", noise="")
+import nltk
+
+
+def main():
+    model, synther = load_model()
+    st.title("Chương trình sửa lỗi chính tả tiếng Việt")
+    # Load model
+    state.input = ""
+    state.noise = ""
+    text_input = st.text_area("Nhập đầu vào:", value=state.input)
+    text_input = text_input.strip()
+    if st.button("Correct"):
+        state.noise = text_input
+        state.text_correct = model.spelling_correct(state.noise)
+        st.text("Câu nhiễu: ")
+        st.success(state.noise)
+        st.text("Kết quả:")
+        st.success(state.text_correct)
+
+    if st.button("Add noise and Correct"):
+        state.noise = synther.add_noise(text_input, percent_err=0.3)
+        # state.output = noise_text
+        state.text_correct = model.spelling_correct(state.noise)
+        st.text("Câu nhiễu: ")
+        st.success(state.noise)
+        st.text("Kết quả:")
+        st.success(state.text_correct)
+
+    # state.sync()
+
+
+@st.cache(allow_output_mutation=True)  # hash_func
+def load_model():
+    print("Loading model ...")
+    nltk.download('punkt')
+    model = Predictor(weight_path='weights/seq2seq.pth', have_att=True)
+    synther = SynthesizeData()
+    return model, synther
+
+
+if __name__ == "__main__":
+    main()
---
+++ params.py
@@ -0,0 +1,28 @@
+print("here")
+
+DEVICE = 'cpu'
+ENC_EMB_DIM = 256
+DEC_EMB_DIM = 256
+ENC_HID_DIM = 512
+DEC_HID_DIM = 512
+ENC_DROPOUT = 0.5
+DEC_DROPOUT = 0.5
+
+NUM_ITERS = 80000
+BATCH_SIZE = 4
+BEAM_SEARCH = False
+PRINT_PER_ITER = 1
+VALID_PER_ITER = 500
+MAX_SAMPLE_VALID = 10000
+
+MAX_LR = 0.0003    # lr will inscrease from 2e-5 to MAX_LR in iter 0 -> iter NUM_ITERS * PCT_START, then decrease to 2e-5
+PCT_START = 0.1
+
+LOG = "./log/loger_luong.log"
+CHECKPOINT = './checkpoint/seq2seq_luong_checkpoint.pth'
+EXPORT = './weights/seq2seq_luong.pth'
+
+MAXLEN = 46
+NGRAM = 5
+alphabets = 'aAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬbBcCdDđĐeEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆfFgGhHiIìÌỉỈĩĨíÍịỊjJkKlLmMnNoOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢpPqQrRsStTuUùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰvVwWxXyYỳỲỷỶỹỸýÝỵỴzZ0123456789!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~ ]'
+PERCENT_NOISE = 0.15 # 0 - 2 word in 5-grams

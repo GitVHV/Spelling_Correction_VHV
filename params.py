f9d72d7ef030b9650f3850b662589bb5cdfc68d0
GitVHV <datnt@vhv.vn>
2022-05-23 15:56:45 +0700

    first commit

--- params.py
+++ params.py
@@ -13,14 +13,14 @@ BATCH_SIZE = 160
 BEAM_SEARCH = False
 PRINT_PER_ITER = 1
 VALID_PER_ITER = 1000
-MAX_SAMPLE_VALID = 16000
+MAX_SAMPLE_VALID = 1600
 
 MAX_LR = 0.0003  # lr will inscrease from 2e-5 to MAX_LR in iter 0 -> iter NUM_ITERS * PCT_START, then decrease to 2e-5
 PCT_START = 0.1
 
-LOG = "/content/gdrive/MyDrive/ocr/Spelling_Correction_Vietnamese/log/loger_luong.log"
-CHECKPOINT = '/content/gdrive/MyDrive/ocr/Spelling_Correction_Vietnamese/checkpoint/seq2seq_luong_checkpoint.pth'
-EXPORT = '/content/gdrive/MyDrive/ocr/Spelling_Correction_Vietnamese/weights/seq2seq_luong.pth'
+LOG = "/content/gdrive/MyDrive/Spelling_Correction_VHV/log/loger_luong.log"
+CHECKPOINT = '/content/gdrive/MyDrive/Spelling_Correction_VHV/checkpoint/seq2seq_luong_checkpoint.pth'
+EXPORT = '/content/gdrive/MyDrive/Spelling_Correction_VHV/weights/seq2seq_luong.pth'
 
 MAXLEN = 46
 NGRAM = 5
